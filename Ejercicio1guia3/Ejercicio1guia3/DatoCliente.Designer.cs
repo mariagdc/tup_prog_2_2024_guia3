cd478a183408ae9b104eaa6b7a28b202ba1633f1
mariagdc <mariagodoydelcastillo@gmail.com>
2024-09-04 17:05:03 -0300

    Agrego Ventanas modales

---
+++ Ejercicio1guia3/Ejercicio1guia3/DatoCliente.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Ejercicio1guia3
+{
+    partial class DatoCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAcepCliente = new System.Windows.Forms.Button();
+            this.btnCancelClient = new System.Windows.Forms.Button();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbDireccion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAcepCliente
+            // 
+            this.btnAcepCliente.Location = new System.Drawing.Point(113, 202);
+            this.btnAcepCliente.Name = "btnAcepCliente";
+            this.btnAcepCliente.Size = new System.Drawing.Size(75, 23);
+            this.btnAcepCliente.TabIndex = 0;
+            this.btnAcepCliente.Text = "Aceptar";
+            this.btnAcepCliente.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelClient
+            // 
+            this.btnCancelClient.Location = new System.Drawing.Point(204, 202);
+            this.btnCancelClient.Name = "btnCancelClient";
+            this.btnCancelClient.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelClient.TabIndex = 1;
+            this.btnCancelClient.Text = "Cancelar";
+            this.btnCancelClient.UseVisualStyleBackColor = true;
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(131, 81);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(190, 20);
+            this.tbNombre.TabIndex = 2;
+            // 
+            // tbDireccion
+            // 
+            this.tbDireccion.Location = new System.Drawing.Point(131, 121);
+            this.tbDireccion.Name = "tbDireccion";
+            this.tbDireccion.Size = new System.Drawing.Size(190, 20);
+            this.tbDireccion.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Dirección";
+            // 
+            // DatoCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 278);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbDireccion);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.btnCancelClient);
+            this.Controls.Add(this.btnAcepCliente);
+            this.Name = "DatoCliente";
+            this.Text = "DatoCliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button btnAcepCliente;
+        public System.Windows.Forms.Button btnCancelClient;
+        public System.Windows.Forms.TextBox tbNombre;
+        public System.Windows.Forms.TextBox tbDireccion;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label label2;
+    }
+}
--- Ejercicio1guia3/Ejercicio1guia3/Models/Presupuesto.cs
+++ Ejercicio1guia3/Ejercicio1guia3/Models/Presupuesto.cs
@@ -8,5 +8,28 @@ namespace Ejercicio1guia3.Models
 {
     internal class Presupuesto
     {
+        public int CantidadProductos {  get; private set; }
+        public double CostoTotal { get; private set; }
+
+        public string Cliente {  get; set; }
+        public string Direccion {  get; set; }
+
+
+        public Presupuesto(string cliente, string direccion)
+        {
+            Cliente = cliente;
+            Direccion = direccion;
+
+        }
+
+        public  AgregarProducto(Producto)
+        {
+
+        }
+
+        public int VerProducto(int idx)
+        {
+            return idx;
+        }
     }
 }
